e0e22fec2d6178a103c7222240fed3acbd848a83
Andrea Moreno <andymorenosky2020@gmail.com>
2022-11-10 15:41:31 -0700

    Mejorar mapa

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([24.799119, -107.342517], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([24.81485435708219, -107.43232829500889]).addTo(map);
