a6523b461a83fa54f54b38d653333ed288b4b0a5
Irlanda <irlandakelly3@gmail.com>
2022-11-28 15:40:48 -0700

    listas todas las capas

--- map.js
+++ map.js
@@ -52,31 +52,109 @@ var uniPriv = L.geoJson(privadas, {
 }).addTo(map);
 
 // Rezago Educativo
-/*
-function getPercentage (feature) {
+
+function getPercentageEdu (feature) {
     if (feature.properties.Porcentaje <= 20) {
         return 1
     }
     else if (feature.properties.Porcentaje <= 27) {
         return 2
     }
+    else if (feature.properties.Porcentaje <= 33) {
+        return 3
+    }
+    else if (feature.properties.Porcentaje <= 40) {
+        return 4
+    }
+    else if (feature.properties.Porcentaje <= 67) {
+        return 5
+    }
 };
 
 var rezagoEdu = L.geoJSON(rezagoEdu, {
     style: function(feature) {
-        switch (getPercentage(feature)) {
-            case 1: return {color: "#ff0000"};
-            case 2: return {color: "#0000ff"};
+        switch (getPercentageEdu(feature)) {
+            case 1: return {color: "#f6cacc", weight:1, fillOpacity:.7}
+            case 2: return {color: "#ec8385", weight:1, fillOpacity:.7}
+            case 3: return {color: "#e35053", weight:1, fillOpacity:.7}
+            case 4: return {color: "#d02224", weight:1, fillOpacity:.7}
+            case 5: return {color: "#9c191b", weight:1, fillOpacity:.7}
         }
     }
-}).addTo(map);
-*/
+});
 
+// Pobreza
+
+function getPercentagePob (feature) {
+    if (feature.properties.inlbpor <= 56) {
+        return 1
+    }
+    else if (feature.properties.inlbpor <= 67) {
+        return 2
+    }
+    else if (feature.properties.inlbpor <= 77) {
+        return 3
+    }
+    else if (feature.properties.inlbpor <= 86) {
+        return 4
+    }
+    else if (feature.properties.inlbpor <= 97) {
+        return 5
+    }
+};
+
+var pobreza = L.geoJSON(pobreza, {
+    style: function(feature) {
+        switch (getPercentagePob(feature)) {
+            case 1: return {color: "#eef6fc", weight:1, fillOpacity:.7}
+            case 2: return {color: "#97caed", weight:1, fillOpacity:.7}
+            case 3: return {color: "#63b0e3", weight:1, fillOpacity:.7}
+            case 4: return {color: "#2280bf", weight:1, fillOpacity:.7}
+            case 5: return {color: "#0f3a57", weight:1, fillOpacity:.7}
+        }
+    }
+});
+
+// Rezago Social
+
+function getPercentageSoc (feature) {
+    if (feature.properties.GRS10 == 'Muy bajo') {
+        return 1
+    }
+    else if (feature.properties.GRS10 == 'Bajo') {
+        return 2
+    }
+    else if (feature.properties.GRS10 == 'Medio') {
+        return 3
+    }
+    else if (feature.properties.GRS10 == 'Alto') {
+        return 4
+    }
+    else if (feature.properties.GRS10 == 'Muy alto') {
+        return 5
+    }
+};
+
+var rezagoSoc = L.geoJSON(rezagoSoc, {
+    style: function(feature) {
+        switch (getPercentageSoc(feature)) {
+            case 1: return {color: "#daf2da", weight:1, fillOpacity:.7}
+            case 2: return {color: "#98d998", weight:1, fillOpacity:.7}
+            case 3: return {color: "#60bf60", weight:1, fillOpacity:.7}
+            case 4: return {color: "#32a632", weight:1, fillOpacity:.7}
+            case 5: return {color: "#008000", weight:1, fillOpacity:.7}
+        }
+    }
+});
+
+// Arreglo final
 
 var capasAdicionales = {
     "Universidades Públicas":uniPub,
     "Universidades Privadas":uniPriv,
-    // "Rezago Educativo":rezagoEdu,
+    "Rezago Educativo":rezagoEdu,
+    "Nivel de pobreza":pobreza,
+    "Rezago Social": rezagoSoc,
 };
 
 var capasBase = {
